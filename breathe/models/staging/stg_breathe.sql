324979fef56f390966f28fd04d7d8a1cdf63f19f
joeraymon <joe.raymon10@gmail.com>
2026-04-10 10:12:01 -0500

    feat: add session_id to stg_breathe, add staging schema.yml

--- breathe/models/staging/stg_breathe.sql
+++ breathe/models/staging/stg_breathe.sql
@@ -5,12 +5,13 @@ with source as (
 renamed as (
     select
         timestamp::timestamp as logged_at,
+        cast(timestamp::timestamp as varchar) as session_id,
         type as entry_type,
         level as severity_level,
         label as severity_label,
-        case 
-            when note = '' then null 
-            else note 
+        case
+            when note = '' then null
+            else note
         end as note
     from source
 )
